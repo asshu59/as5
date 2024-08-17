940a5b4621c9cc57f17a4909094cbc78154535a4
asshu59 <159796553+asshu59@users.noreply.github.com>
2024-08-17 03:05:28 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("asss","asss")
+        ERC721("ashima","asm")
         Ownable(initialOwner)
     {}
 
